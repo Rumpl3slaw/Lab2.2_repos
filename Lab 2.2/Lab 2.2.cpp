05306f3a9918d05dfc419c5a66702b7ee6df0ae8
Rumpl3slaw <wowpanda13@ukr.net>
2020-10-11 16:26:48 +0300

    first masterr commit

---
+++ Lab 2.2/Lab 2.2.cpp
@@ -0,0 +1,25 @@
+﻿// Lab2.1.cpp 
+// < Демчук Роман >
+// Лабораторна робота № 2.1. 
+// Лінійні програми. 
+// Варіант 5
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	double a;
+	double z1;
+	//double z2;
+
+	cout << "a="; cin >> a;
+
+	z1 = 1 - 1. / 4 * sin(2 * a) * sin(2 * a) + cos(2 * a);
+	//z2 = cos(a) * cos(a) + cos(a) * cos(a) * cos(a) * cos(a);
+
+	cout << "z1=" << z1 << endl;
+	//cout << "z2=" << z2 << endl;
+
+	return 0;
+}
