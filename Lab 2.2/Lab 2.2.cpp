aaeda2676d01276e120abe59280034f263e061eb
Rumpl3slaw <wowpanda13@ukr.net>
2020-10-11 16:39:16 +0300

    first branch_02 commit

--- Lab 2.2/Lab 2.2.cpp
+++ Lab 2.2/Lab 2.2.cpp
@@ -10,16 +10,16 @@ using namespace std;
 int main()
 {
 	double a;
-	double z1;
-	//double z2;
+	//double z1;
+	double z2;
 
 	cout << "a="; cin >> a;
 
-	z1 = 1 - 1. / 4 * sin(2 * a) * sin(2 * a) + cos(2 * a);
-	//z2 = cos(a) * cos(a) + cos(a) * cos(a) * cos(a) * cos(a);
+	//z1 = 1 - 1. / 4 * sin(2 * a) * sin(2 * a) + cos(2 * a);
+	z2 = cos(a) * cos(a) + cos(a) * cos(a) * cos(a) * cos(a);
 
-	cout << "z1=" << z1 << endl;
-	//cout << "z2=" << z2 << endl;
+	//cout << "z1=" << z1 << endl;
+	cout << "z2=" << z2 << endl;
 
 	return 0;
 }
